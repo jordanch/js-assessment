5678bd8ed4a9ae9e7d127097be86021764ba9ebc
Jordan Hunt <jordan@gli-sa.co.za>
2017-06-11 18:37:00 +0200

    count and bestPractices done.

--- app/count.js
+++ app/count.js
@@ -2,6 +2,20 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.countAnswers = {
   count: function (start, end) {
-
+    console.log(start);
+    var increment = start,
+      obj = {},
+      interval = window.setInterval(function () {
+        if (increment < end) {
+          increment += 1;
+          console.log(increment);
+        } else {
+          window.clearInterval(interval);
+        }
+      }, 100);
+    obj.cancel = function () {
+      window.clearInterval(interval);
+    };
+    return obj;
   }
 };
